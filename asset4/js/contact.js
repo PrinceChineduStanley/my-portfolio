f078ed3a315f1819f6f78a3c09f98df5438b1b20
PrinceChineduStanley <okoriechinedustanley@gmail.com>
2024-10-27 18:19:28 +0100

    changes

--- asset4/js/contact.js
+++ asset4/js/contact.js
@@ -1,19 +1,17 @@
 const cancel1 =document.getElementById("cancel4")
-
-
-
-addEventListener("click", goback4);
-
-function goback4(){
-    location.href = "index.html"
-}
-
-
-
-
 const putname =document.getElementById("putname")
 const putemail =document.getElementById("putemail")
 const putmessage =document.getElementById("putmessage")
+
+
+
+function goback4(){
+
+        addEventListener("click", goback4);
+
+    location.href = "index.html"
+}
+
 
 
 function sendbutton(){
@@ -21,12 +19,22 @@ function sendbutton(){
         let num2 = putemail.value
         let num3 = putmessage.value
 
+        if (num1==putname.value){
+            sendbutton=true
+        }
     
-        if(num1==putname.value +num2==putemail.value +num3==putmessage.value){
-            "Sent successfully"
+            
         }
     
     
-}
+
+
+
+
+
+
+
+
+
 
 
